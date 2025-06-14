5ec91da1c248e138b3ff115e362f0494a3240fe6
さがしマート <163527251+sagashi0120@users.noreply.github.com>
2025-06-14 16:33:39 +0900

    Delete php directory

--- php/session.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-error_reporting(0);
-$_SESSION["blog-manager-login"] = true;
-if(!isset($_SESSION["file-manager-login"])) {
-    header("Location: ./login.php");
-    exit();
-}
-?>
